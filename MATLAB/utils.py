d48c5b74073575b518481fa8069ddcd5f86a4f5b
Marinus Pennings <pennings@tamu.edu>
2025-05-25 17:34:31 -0500

    Created MATLAB env that can be used on all tamu clusters. Cleaned up older envs

--- MATLAB/utils.py
+++ MATLAB/utils.py
@@ -1,6 +1,11 @@
 import os
 import subprocess
 
+
+def retrieve_cluster():
+     result = subprocess.run( ['bash', '-c', 'clustername'],capture_output=True,text=True,check=False)
+     return result.stdout.strip()
+
 def retrieve_main(mainscript=""):
     if (mainscript != ""  ):
         return  os.path.basename(mainscript)
@@ -15,28 +20,35 @@ def  retrieve_matlabpath(mainscript,matlabroot):
         path_dir="export MATLABPATH="+path_dir+":$MATLABPATH"
     return path_dir	
     
-def check_matlab_group():
-    #check if user is in the MATLAB group
-    matlab_group = "matlab"
-    user_name = os.getenv("USER")
-    
-    #fetch group information for the matlab group
-    try:
-        group_info = subprocess.check_output(f"grep {matlab_group} /etc/group", shell=True).decode("utf-8")
-    except subprocess.CalledProcessError:
-        raise ValueError(f"Group '{matlab_group}' not found in /etc/group")
-    
-    #output is usually in the format: group_name:x:group_id:user1,user2,...
-    group_members = group_info.split(":")[-1].strip().split(",")
-    
-    #check if the current user is in the MATLAB group
-    if user_name in group_members:
+def allow_matlab():
+    # On ACES, matlab is group protected. We will check if this Matlab env is on ACES and if so
+    # check if the user is in the matlab group
+
+    #check if user is in the MATLAB group on ACES
+    cluster = retrieve_cluster();
+    if cluster != "aces":
         return True
     else:
-        return False
+        matlab_group = "matlab"
+        user_name = os.getenv("USER")
+    
+        #fetch group information for the matlab group
+        try:
+            group_info = subprocess.check_output(f"grep {matlab_group} /etc/group", shell=True).decode("utf-8")
+        except subprocess.CalledProcessError:
+            raise ValueError(f"Group '{matlab_group}' not found in /etc/group")
+    
+        #output is usually in the format: group_name:x:group_id:user1,user2,...
+        group_members = group_info.split(":")[-1].strip().split(",")
+    
+        #check if the current user is in the MATLAB group
+        if user_name in group_members:
+            return True
+        else:
+            return False
     
 def retrieve_matlabopts(job_name,workers,threads,walltime,memory,gpu):
-    if not check_matlab_group():
+    if not allow_matlab():
         drona_add_warning("Matlab is restricted software on ACES. If you are associated with an educational organization, please Contact HPRC to be added to the access list.")
     
     options_string=""
